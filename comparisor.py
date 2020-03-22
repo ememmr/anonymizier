099de155b630877833b7b35db34eb230147106c1
mrtmmr <marta.mtnezdemurgar@gmail.com>
2020-03-22 12:29:24 +0100

    Treat each df separated

--- comparisor.py
+++ comparisor.py
@@ -1,25 +1,19 @@
 from utils import splitter
 
 
-def measure(data_orig, data_anon, train_size, model, validator):
+def measure(data, train_size, model, validator):
     test_size = 1 - train_size
 
     # Let's split data for each case
-    train_anon, test_anon = splitter(data_anon, train_size, test_size)
-    train_orig, test_orig = splitter(data_orig, train_size, test_size)
+    train, test = splitter(data, train_size, test_size)
 
     # Then fit model and predict
-    model.fit(X=train_anon.iloc[:, :-1], y=train_anon.iloc[:, -1])
-    predictions_anon = model.predict(X=test_anon.iloc[:, :-1])
-
-    model.fit(X=train_orig.iloc[:, :-1], y=train_orig.iloc[:, -1])
-    predictions_orig = model.predict(X=test_orig.iloc[:, :-1])
+    model.fit(X=train.iloc[:, :-1], y=train.iloc[:, -1])
+    predictions = model.predict(X=test.iloc[:, :-1])
 
     # Compare results, who has worse result?
-    error_anon = validator(test_anon.iloc[:, -1], predictions_anon)
-    error_orig = validator(test_orig.iloc[:, -1], predictions_orig)
+    error = validator(test.iloc[:, -1], predictions)
 
-    print("For data anonimizied, the error is: " + str(error_anon))
-    print("For full data, the error is: " + str(error_orig))
+    print("The error is: " + str(error))
 
-    return error_anon, error_orig
+    return error
--- utils.py
+++ utils.py
@@ -6,3 +6,11 @@ def splitter(data, train, test):
         return train_test_split(data, train_size=train, test_size=test)
     else:
         print("Use all the data! train + test must be 1.0")
+
+
+def exampleCleaner(df):
+    df['age'] = df['age'].astype(int)
+    df['cp'] = df['cp'].replace('\*', '', regex=True).astype(int)
+    df['gender'] = df['gender'].replace('F', '1', regex=True).replace('M', '0', regex=True).astype(int)
+    df['got_virus'] = df['got_virus'].replace('y', '1', regex=True).replace('n', '0', regex=True).astype(int)
+    return df
